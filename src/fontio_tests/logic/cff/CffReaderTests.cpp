40a5d867786bb44e78f530b02f2167c5512a5a8f
Mikhail Balakhno <mikhail.balakhno@yaliny.com>
2015-03-06 11:42:52 +0300

    Added licencse clause to all sources. Accomodated MSVC porting changes in unit tests.

--- src/fontio_tests/logic/cff/CffReaderTests.cpp
+++ src/fontio_tests/logic/cff/CffReaderTests.cpp
@@ -1,3 +1,33 @@
+// The Modified BSD License
+//
+// Copyright (c) 2015, Mikhail Balakhno
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of fontio nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #include <fstream>
 
 #include <gtest/gtest.h>
@@ -17,7 +47,7 @@ namespace fontio { namespace logic { namespace cff
 
     protected:
 
-        Cff ReadFile(const std::string& filename)
+        std::unique_ptr<Cff> ReadFile(const std::string& filename)
         {
             std::ifstream stream(filename, std::ios_base::binary);
 
@@ -48,27 +78,27 @@ namespace fontio { namespace logic { namespace cff
     {
         auto cff = this->ReadFile("test_data/cff/bare.cff");
 
-        ASSERT_EQ(1, cff.GetHeader().GetVersionMajor());
-        ASSERT_EQ(0, cff.GetHeader().GetVersionMinor());
-        ASSERT_EQ(4, cff.GetHeader().GetHeaderSize());
-        ASSERT_EQ(2, cff.GetHeader().GetOffsetSize());
+        ASSERT_EQ(1, cff->GetHeader().GetVersionMajor());
+        ASSERT_EQ(0, cff->GetHeader().GetVersionMinor());
+        ASSERT_EQ(4, cff->GetHeader().GetHeaderSize());
+        ASSERT_EQ(2, cff->GetHeader().GetOffsetSize());
     }
 
     TEST_F(CffReaderTests, CanReadNameIndex)
     {
         auto cff = this->ReadFile("test_data/cff/bare.cff");
 
-        ASSERT_EQ(1, cff.GetNameIndex().GetNames().size());
-        ASSERT_EQ("GuardianSans-Regular", cff.GetNameIndex().GetNames()[0]);
+        ASSERT_EQ(1, cff->GetNameIndex().GetNames().size());
+        ASSERT_EQ("GuardianSans-Regular", cff->GetNameIndex().GetNames()[0]);
     }
 
     TEST_F(CffReaderTests, CanReadTopDict)
     {
         auto cff = this->ReadFile("test_data/cff/bare.cff");
 
-        ASSERT_EQ(1, cff.GetTopDicts().size());
+        ASSERT_EQ(1, cff->GetTopDicts().size());
 
-        const auto& topDict = cff.GetTopDicts()[0];
+        const auto& topDict = cff->GetTopDicts()[0];
 
         this->AssertFontMatrixEqual(CffFontMatrix(0.001, 0.0, 0.0, 0.001, 0.0, 0.0), topDict.GetFontMatrix());
 
@@ -81,8 +111,8 @@ namespace fontio { namespace logic { namespace cff
     {
         auto cff = this->ReadFile("test_data/cff/bare.cff");
 
-        const auto& strings = cff.GetStringIndex();
-        const auto& topDict = cff.GetTopDicts()[0];
+        const auto& strings = cff->GetStringIndex();
+        const auto& topDict = cff->GetTopDicts()[0];
 
         const auto& charset = topDict.GetCharset();
 
@@ -96,7 +126,7 @@ namespace fontio { namespace logic { namespace cff
     {
         auto cff = this->ReadFile("test_data/cff/test_font_8.cff");
 
-        const auto& topDict = cff.GetTopDicts()[0];
+        const auto& topDict = cff->GetTopDicts()[0];
 
         topDict.GetCharset();
     }
